fa8acb19329ef29ab9316b9a0b46e1e5ba5f8f71
Nazar <nazardovhanchuk@gmail.com>
2021-05-13 14:15:13 +0300

    add change

--- src/components/video/video.component.js
+++ src/components/video/video.component.js
@@ -2,11 +2,12 @@ import React from "react";
 import Plyr from 'plyr-react'
 import 'plyr-react/dist/plyr.css'
 
-import { getVideoSource } from 'utils/video.utils'
+import { getVideoSource } from 'utils/video.utils';
+import clsx from 'clsx';
 
 const Video = ({ url, className }) => {
   return (
-    <div className={className}>
+    <div className={clsx('video-component', className)}>
       <Plyr source={getVideoSource(url)} />
     </div>
   )
