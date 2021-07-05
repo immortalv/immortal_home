cdf82364e59ba2e72290426f17357743b51cba59
Andrii Tykhan <andriytixan@gmail.com>
2021-07-05 17:36:51 +0300

    Add profiles debounce

--- src/pages/profiles/profiles.page.js
+++ src/pages/profiles/profiles.page.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import debounce from "lodash.debounce";
+import useDebounceValue from "hooks/use-debounce-value.hook";
 import { PROFILE } from "constants/routes.constants";
 import { useProfiles } from "queries/profiles/use-profiles";
 import ProfilesCard from "components/profiles/profiles-card";
@@ -8,32 +8,15 @@ import SearchContainer from "components/common/search-input";
 import { validateProfileSearchValue } from "utils/profile.utils";
 
 import "./style.scss";
-// const DEBOUNCE_TIME = 400;
-
-// const defaultOptions = {
-//   leading: false,
-//   trailing: true,
-// };
-
-// const useDebouncedFn = (
-//   fn,
-//   wait = 100,
-//   options = defaultOptions,
-//   dependencies
-// ) => {
-//   console.log("dependencies", dependencies);
-//   const debounced = debounce(fn, wait, options);
-
-//   return useCallback(debounced, dependencies);
-// };
 
 const ProfilesPage = () => {
-  const [search, setSearch] = useState("");
-  const { data } = useProfiles(search);
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounceValue(searchTerm);
+  const { data } = useProfiles(debouncedSearchTerm);
 
   const handleChange = ({ target }) => {
     const validated = validateProfileSearchValue(target.value);
-    setSearch(validated);
+    setSearchTerm(validated);
   };
 
   return (
@@ -47,7 +30,7 @@ const ProfilesPage = () => {
             Оживіть спогади вашої родини, досліджуючи життя тих, хто був до вас.
           </p>
         </div>
-        <SearchContainer value={search} onChange={handleChange} />
+        <SearchContainer value={searchTerm} onChange={handleChange} />
         <div className="profiles__list">
           <h3 className="title">Публічні профілі</h3>
           {data?.results?.map((profile, index) => (
