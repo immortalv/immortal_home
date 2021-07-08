eb637c50599174ac3805110e260b681abf236aab
Andrii Tykhan <andriytixan@gmail.com>
2021-07-08 08:44:46 +0300

    Update profiles

--- src/components/profiles/add-profile/steps/add-media.component.js
+++ src/components/profiles/add-profile/steps/add-media.component.js
@@ -5,6 +5,9 @@ import { CrossIconSolid } from "icons";
 
 import "./style.scss";
 
+const getFileName = (file) => file.name || file.originalName || file.fileName;
+const getFileKey = (file) => file.id || file.name || file.fileName;
+
 const AddMedia = ({ className, title, label, files, addFiles }) => {
   const onOtherMediaDrop = (settledFiles) => {
     const fileNames = files.map((file) => file.name);
@@ -19,10 +22,11 @@ const AddMedia = ({ className, title, label, files, addFiles }) => {
   };
 
   const removeFile = (fileName) => {
-    const filteredFiles = files.filter((file) => file.name !== fileName);
+    const filteredFiles = files.filter((file) => getFileKey(file) !== fileName);
     addFiles(filteredFiles);
   };
 
+  console.log('files', files);
   return (
     <div className={clsx("add-media__container", className)}>
       {title && <h2 className="header-s-2 add-profile__subtitle">{title}</h2>}
@@ -37,11 +41,11 @@ const AddMedia = ({ className, title, label, files, addFiles }) => {
 
         <ul className="file-list">
           {files.map((file) => (
-            <li key={file.name || file.fileName} className="file-list__item">
-              {file.name || file.fileName}
+            <li key={getFileKey(file)} className="file-list__item">
+              {getFileName(file)}
               <button
                 className="file-list__btn"
-                onClick={() => removeFile(file.name)}
+                onClick={() => removeFile(getFileKey(file))}
               >
                 <CrossIconSolid className="file-list__btn-icon" />
               </button>
